842f3eae3f8ed21f696afb7c927889129d95ac0a
Mikel Najera Lorenzo <mnajeral@educacion.navarra.es>
2020-05-15 22:50:45 +0200

    Add some tests related to add method of the Calculator. Implement add method

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,6 @@ class Calculadora:
     def __init__(self):
         self.value = 0
 
+    def add(self, a, b):
+        self.value = a + b
+
--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -8,3 +8,12 @@ class TestCaseCalculadora(unittest.TestCase):
         calc = Calculadora()
         self.assertEqual(0, calc.value)
 
+    def test_add_2_and_3(self):
+        calc = Calculadora()
+        calc.add(2, 3)
+        self.assertEqual(5, calc.value)
+
+    def test_add_4_and_6(self):
+        calc = Calculadora()
+        calc.add(4, 6)
+        self.assertEqual(10, calc.value)
