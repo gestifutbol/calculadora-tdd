e00bd1616e771600af596a59c061e237695a62ef
Mikel Najera Lorenzo <mnajeral@educacion.navarra.es>
2020-05-15 22:55:08 +0200

    Refactor: Calculator instance moved out to setUp method

--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -4,16 +4,16 @@ from calculadora import Calculadora
 
 class TestCaseCalculadora(unittest.TestCase):
 
+    def setUp(self):
+        self.calc = Calculadora()
+
     def test_initial_value(self):
-        calc = Calculadora()
-        self.assertEqual(0, calc.value)
+        self.assertEqual(0, self.calc.value)
 
     def test_add_2_and_3(self):
-        calc = Calculadora()
-        calc.add(2, 3)
-        self.assertEqual(5, calc.value)
+        self.calc.add(2, 3)
+        self.assertEqual(5, self.calc.value)
 
     def test_add_4_and_6(self):
-        calc = Calculadora()
-        calc.add(4, 6)
-        self.assertEqual(10, calc.value)
+        self.calc.add(4, 6)
+        self.assertEqual(10, self.calc.value)
